961c03ec1a0be9cac278e00380022296da72ecd7
nxcxlxs <nick_mro2@hotmail.com>
2024-07-29 21:14:12 -0300

    Update get_scopus_publications[NICO].R

--- get_scopus_publications[NICO].R
+++ get_scopus_publications[NICO].R
@@ -3,15 +3,15 @@ library(httr)
 library(jsonlite)
 library(openxlsx)
 
-#get publication details from Scopus with multiple keywords and year filters
+# get publication details from Scopus with multiple keywords and year filters
 get_scopus_publications <- function(keywords, api_key, start_year = NULL,
                                     end_year = NULL, start_index = 0, count = 25) {
   url <- "https://api.elsevier.com/content/search/scopus"
   
-  #combine keywords using AND
+  # combine keywords using AND
   query_string <- paste(keywords, collapse = " AND ")
   
-  #add year filters to the query string
+  # add year filters to the query string
   if (!is.null(start_year) && !is.null(end_year)) {
     query_string <- paste0(query_string, " AND PUBYEAR AFT ", start_year - 1,
                            " AND PUBYEAR BEF ", end_year + 1)
@@ -21,7 +21,7 @@ get_scopus_publications <- function(keywords, api_key, start_year = NULL,
     query_string <- paste0(query_string, " AND PUBYEAR BEF ", end_year + 1)
   }
   
-  #API request
+  # API request
   response <- GET(url, 
                   add_headers(`X-ELS-APIKey` = api_key),
                   query = list(query = query_string,
@@ -40,21 +40,21 @@ get_scopus_publications <- function(keywords, api_key, start_year = NULL,
   return(json_data)
 }
 
-#set parameters
+# set parameters
 api_key <- "xXxXxXxXxXxXxXxXxXxXxXxXxXx"  #replace with valid API key
 keywords <- c("soil", "microplastics", "coastal")
 start_year <- 2022
 end_year <- 2023
 
-#get publication data
+# get publication data
 json_data <- get_scopus_publications(keywords, api_key, start_year, end_year)
 
-#extract authors, titles and DOIs
+# extract authors, titles and DOIs
 author <- json_data$`search-results`$entry$`dc:creator`
 title <- json_data$`search-results`$entry$`dc:title`
 doi <- json_data$`search-results`$entry$`prism:doi`
 
-#data frame
+# data frame
 article_data <- data.frame(
   Title = title,
   DOI = doi,
@@ -64,7 +64,7 @@ article_data <- data.frame(
   stringsAsFactors = FALSE
 )
 
-#write spreadsheet
+# write spreadsheet
 write.xlsx(article_data, paste0("scopus_articles_",
                                 start_year, "-",
                                 end_year, ".xlsx"))
